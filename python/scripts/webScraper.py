ffc1fe3baa58de0c2d79b6e49ec1ba447c49992b
Delano Usiukiewicz <delanou@ou.edu>
2018-10-15 21:40:34 -0500

    message

--- python/scripts/webScraper.py
+++ python/scripts/webScraper.py
@@ -2,11 +2,21 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-
 url = sys.argv[1]
 
-rawContent = requests.get(url).content
-print(url)
+requestObject = requests.get(url)
+html = requestObject.text
+soup = BeautifulSoup(html, 'html.parser')
+#print(soup.prettify().encode('UTF-8'))
+print(soup.title)
 
-html = BeautifulSoup(rawContent, 'html.parser')
-print(html)
+# from here get the title
+# pledged amount
+# goal
+# backers
+# deadline month
+
+# can get city -> make api call to determine country -> make api call to get abbreviation
+
+# then navigate to url + /faq
+# from here get the launched month
